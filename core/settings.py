1913605a5d6a6d39b04c02e9392e74bce1c6a2db
sehamanter1 <sehamanter2@gmail.com>
2026-04-29 21:21:14 +0300

    Refactor: comment out cloudinary configuration for deployment - temporary

--- core/settings.py
+++ core/settings.py
@@ -111,12 +111,12 @@ DATABASES = {
     )
 }
 
-cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-    secure=True
-)
+# cloudinary.config(
+#     cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+#     api_key=os.getenv("CLOUDINARY_API_KEY"),
+#     api_secret=os.getenv("CLOUDINARY_API_SECRET"),
+#     secure=True
+# )
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
